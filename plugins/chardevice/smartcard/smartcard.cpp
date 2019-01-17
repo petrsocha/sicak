d7b10d7b2b48d75ac218b8e943fa5b20aa0c94b6
Petr Socha <sochapet@fit.cvut.cz>
2019-01-17 03:31:40 +0100

    licence year update

--- plugins/chardevice/smartcard/smartcard.cpp
+++ plugins/chardevice/smartcard/smartcard.cpp
@@ -1,6 +1,6 @@
 /*
 *  SICAK - SIde-Channel Analysis toolKit
-*  Copyright (C) 2018 Petr Socha, FIT, CTU in Prague
+*  Copyright (C) 2019 Petr Socha, FIT, CTU in Prague
 *
 *  This program is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
