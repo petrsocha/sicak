970c1d267581eb272a9a66f25f5e1109f972f865
Petr Socha <46137159+petrsocha@users.noreply.github.com>
2020-03-10 23:45:52 +0100

    Assert number of samples received in rapid block mode

--- plugins/oscilloscope/ps6000/ps6000.cpp
+++ plugins/oscilloscope/ps6000/ps6000.cpp
@@ -356,7 +356,7 @@ size_t Ps6000::getValues(int channel, int16_t * buffer, size_t len, size_t & sam
         samples_tmp *= m_captures;
         
         status = ps6000GetValuesBulk(m_handle, &samples_tmp, 0, m_captures - 1, 0, PS6000_RATIO_MODE_NONE, over.get());          
-        if (status) throw RuntimeException("Failed to receive the data");            
+        if (status || samples_tmp != ((m_preTriggerSamples + m_postTriggerSamples)*m_captures)) throw RuntimeException("Failed to receive the data");            
         
         captures = m_captures;
         
